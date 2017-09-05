a228e8a5ba213ba97bee61991c628f5ded2a611f
plainnany <plainnany@gmail.com>
2017-09-05 09:15:48 +0800

    使用webpack-dev-server

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 function component(){
     var element = $('<div></div>');
     /* lodash is required for the next line to work */
-    element.html(_.join(['Hello','webpack and jquery'], ' '));
+    element.html(_.join(['Hello','webpack lalalalahhahhahha'], ' '));
     console.log('方方的教程')
     return element.get(0);
     
